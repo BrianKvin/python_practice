5bbda540bef9c768b843b0e58698b4ace4521904
BrianKvin <briankvin@gmail.com>
2025-05-06 10:13:22 +0300

    o2 Day variables & build in functions

---
+++ python_challenges/30-days-of-Python/02_day.py
@@ -0,0 +1,8 @@
+# Variables and built-in functions
+# A function to calculate the area of a circle of radius 30m
+
+def area_of_a_circle(r, pi=3.14):
+  return pi * r**2
+
+area = area_of_a_circle(30)
+print(area)
