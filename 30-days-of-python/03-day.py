717c2e08752dfa3666efe3d2a9b80eeedeb433a1
BrianKvin <briankvin@gmail.com>
2025-05-07 06:25:27 +0300

    Day 03 - Operators

---
+++ 30-days-of-python/03-day.py
@@ -0,0 +1,16 @@
+# Calculating area of a circle
+radius = 10                                 # radius of a circle
+area_of_circle = 3.14 * radius ** 2         # two * sign means exponent or power
+print('Area of a circle:', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle:', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')
