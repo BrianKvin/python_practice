186e9a350a24f9ed3dc0aa2b2da5eb930047e4f3
BrianKvin <briankvin@gmail.com>
2025-05-08 04:25:50 +0300

    Day 6 - Tuple

---
+++ 30-days-of-python/06-day.py
@@ -0,0 +1,13 @@
+# Tuples
+# Creating a tuple
+# syntax
+empty_tuple = ()
+# or using the tuple constructor
+empty_tuple = tuple()
+#tuple with initial values
+
+tpl = ('item1', 'item2', 'item3','item4')
+
+# converting a list to a tuple
+new_list= list(tpl)
+print(new_list)
