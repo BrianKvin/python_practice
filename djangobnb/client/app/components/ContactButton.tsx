1b87f603cbe14c382bae5bfe597722cfd65287bc
BrianKvin <briankvin@gmail.com>
2025-05-27 09:14:45 +0300

    Landlord section

---
+++ djangobnb/client/app/components/ContactButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactButton = () => {
+  return (
+    <div className="py-4 px-6 bg-airbnb cursor-pointer text-white rounded-xl hover:bg-airbnb-dark transition">
+      Contact
+    </div>
+  );
+}
+
+export default ContactButton;
--- djangobnb/client/app/components/properties/PropertyList.tsx
+++ djangobnb/client/app/components/properties/PropertyList.tsx
@@ -3,8 +3,7 @@ import PropertyListItem from './PropertyListItem'
 
 const PropertyList = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-6">
-      <PropertyListItem />
+    <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
       <PropertyListItem />
       <PropertyListItem />
       <PropertyListItem />
