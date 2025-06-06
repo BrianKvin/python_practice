ede67a8144b33737aef979ff62d80f5d7fcc8d4d
briankvin <briankvin@gmail.com>
2025-06-06 15:04:42 +0300

    chats details page

---
+++ djangobnb/client/app/inbox/[id]/page.tsx
@@ -0,0 +1,12 @@
+import ConversationDetails from '@/app/components/inbox/ConversationDetails';
+import React from 'react'
+
+const ConversationPage = () => {
+  return (
+    <div className="max-w-[1500px] mx-auto px-6 pb-6">
+      <ConversationDetails />
+    </div>
+  )
+}
+
+export default ConversationPage;
