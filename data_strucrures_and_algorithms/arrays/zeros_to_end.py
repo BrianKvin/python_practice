350ca1662fc453b6f01d7ba68206a0526555baa2
briankvin <briankvin@gmail.com>
2025-06-24 05:36:12 +0300

    move all zeros to end

---
+++ data_strucrures_and_algorithms/arrays/zeros_to_end.py
@@ -0,0 +1,16 @@
+# Move all zeros to end 
+def pushZerostoEnd(arr):
+  count = 0
+
+  for i in range(len(arr)):
+    if arr[i] != 0:
+      arr[i], arr[count] = arr[count], arr[i]
+
+      count += 1
+
+if __name__ == "__main__":
+  arr = [1, 2, 0, 3, 4, 0, 5, 0]
+  pushZerostoEnd(arr)
+  # for num in arr:
+  #   print(num, end=' ' )
+  # print()
